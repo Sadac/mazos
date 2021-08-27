4f1d26f16a26a520e7f6da2bf4dce5cc98b47a03
victor sada <victorjsada@gmail.com>
2021-08-26 23:54:41 -0300

    agregando cors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-const express = require('express');
-require('dotenv').config();
-
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
-app.use('/api/usuario', require('./routes/user'));
-app.use('/api/mazo', require('./routes/mazo'));
-app.use('/api/tarjeta', require('./routes/tarjeta'));
-app.use('/api/medalla', require('./routes/medalla'));
+app.use(cors());
+app.use("/api/usuario", require("./routes/user"));
+app.use("/api/mazo", require("./routes/mazo"));
+app.use("/api/tarjeta", require("./routes/tarjeta"));
+app.use("/api/medalla", require("./routes/medalla"));
 
 app.use((error, req, res) => {
   res.status(error.status);
