0bb57bf04d04d8ded7c979f4a829b85fc719668d
victor sada <victorjsada@gmail.com>
2021-08-31 14:38:50 -0300

    set .env

--- src/index.js
+++ src/index.js
@@ -1,26 +1,18 @@
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+app.set("port", process.env.PORT || 4000);
 app.use(cors());
 app.use(express.json());
 app.use(cors());
-app.use('/api/usuario', require('./routes/user'));
-app.use('/api/mazo', require('./routes/mazo'));
-app.use('/api/tarjeta', require('./routes/tarjeta'));
-app.use('/api/medalla', require('./routes/medalla'));
+app.use("/api/usuario", require("./routes/user"));
+app.use("/api/mazo", require("./routes/mazo"));
+app.use("/api/tarjeta", require("./routes/tarjeta"));
+app.use("/api/medalla", require("./routes/medalla"));
 
-// app.use((error, req, res) => {
-//   res.status(error.status);
-//   res.json({
-//     status: error.status,
-//     message: error.message,
-//     stack: error.stack,
-//   });
-// });
-app.listen(PORT, () => {
-  console.log(`Servidor esta levantado en el puerto ${PORT}`);
+app.listen(app.get("port"), () => {
+  console.log(`Servidor esta levantado en el puerto ${app.get("port")}`);
 });
